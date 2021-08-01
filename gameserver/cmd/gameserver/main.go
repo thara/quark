125fca1f1957fcf262d285633998f3ff86e6e857
thara <git@thara.dev>
2021-08-01 16:18:41 +0900

    :muscle: Shutdown gracefull

--- gameserver/cmd/gameserver/main.go
+++ gameserver/cmd/gameserver/main.go
@@ -4,6 +4,9 @@ import (
 	"flag"
 	"log"
 	"net"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"google.golang.org/grpc"
 
@@ -20,6 +23,9 @@ func init() {
 func main() {
 	flag.Parse()
 
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, syscall.SIGTERM, os.Interrupt)
+
 	lis, err := net.Listen("tcp", addr)
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
@@ -28,5 +34,16 @@ func main() {
 	var opts []grpc.ServerOption
 	grpcServer := grpc.NewServer(opts...)
 	gameserver.RegisterRoomServer(grpcServer, room.NewRoomServer())
-	grpcServer.Serve(lis)
+
+	go func() {
+		log.Printf("gRPC service listen at %s", addr)
+		if err := grpcServer.Serve(lis); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	<-sig
+
+	grpcServer.GracefulStop()
+	log.Println("gRPC server shutdown")
 }
