fd16712b069429dfa64bab238cc69707a1f73020
thara <git@thara.dev>
2021-08-07 21:18:50 +0900

    :fire: Remove primitive.go

--- actor.go
+++ actor.go
@@ -1,5 +1,17 @@
 package quark
 
+import "github.com/google/uuid"
+
+type ActorID string
+
+func NewActorID() ActorID {
+	return ActorID(uuid.Must(uuid.NewRandom()).String())
+}
+
+func (a ActorID) String() string {
+	return string(a)
+}
+
 type Actor struct {
 	id ActorID
 
